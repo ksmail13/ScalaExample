b9cc8d17c08aa5b00b9243d35998853a6e6f130b
김민규 <ksmail13@gmail.com>
2017-11-22 02:23:29 +0900

    refactoring state

--- src/main/scala/chap6/State.scala
+++ src/main/scala/chap6/State.scala
@@ -7,25 +7,25 @@ object State {
 
   def unit[S, A](a: A): State[S, A] = s => (a, s)
 
-  def mapNaive[S, A, B](s: State[S, A])(f: A => B): State[S, B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
+  def mapNaive[S, A, B](st: State[S, A])(f: A => B): State[S, B] =
+    s => {
+      val (a, s2) = st(s)
+      (f(a), s2)
     }
 
   def map2Naive[S, A, B, C](ra: State[S, A], rb: State[S, B])(f: (A, B) => C): State[S, C] =
-    rng => {
-      val t1 = ra(rng)
+    s => {
+      val t1 = ra(s)
       val t2 = rb(t1._2)
       (f(t1._1, t2._1), t2._2)
     }
 
   def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
-    rng => {
+    s => {
       def function(fs: List[State[S, A]]): (List[A], S) =
         fs match {
           case h :: Nil =>
-            val result = h(rng)
+            val result = h(s)
             (List(result._1), result._2)
           case h :: t =>
             val result = function(t)
@@ -37,30 +37,30 @@ object State {
     }
 
   def sequenceLoop[S, A](fs: List[State[S, A]]): State[S, List[A]] =
-    rng => {
+    s => {
       @tailrec
-      def loop(fs: List[State[S, A]], rng: S, l: List[A]): (List[A], S) =
+      def loop(fs: List[State[S, A]], s: S, l: List[A]): (List[A], S) =
         fs match {
           case h :: Nil =>
-            val result = h(rng)
+            val result = h(s)
             (result._1 :: l, result._2)
           case h :: t =>
-            val result = h(rng)
+            val result = h(s)
             loop(t, result._2, result._1 :: l)
         }
 
-      loop(fs, rng, List())
+      loop(fs, s, List())
     }
 
   def flatMap[S, A, B](f:State[S, A])(g:A=> State[S, B]): State[S, B] =
-    rng => {
-      val result = f(rng)
+    s => {
+      val result = f(s)
       g(result._1)(result._2)
     }
 
   def map[S, A, B](s: State[S, A])(f: A => B): State[S, B] =
-    flatMap(s)(a => rng => (f(a), rng))
+    flatMap(s)(a => s => (f(a), s))
 
   def map2[S, A, B, C](ra: State[S, A], rb: State[S, B])(f: (A, B) => C): State[S, C] =
-    flatMap(ra)(a => flatMap(rb)(b => rng => (f(a, b), rng)))
+    flatMap(ra)(a => flatMap(rb)(b => s => (f(a, b), s)))
 }
